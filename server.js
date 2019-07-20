35bafecf461a8e6959186e387e480b03fe70caca
remon <remonfawzi0@gmail.com>
2019-07-20 19:03:58 +0200

    Final edits for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,15 +10,21 @@ connectDB();
 // INIT Middleware that allows us to get req JSON data via (req.body)
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('API Running');
-});
-
 // routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/post'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Declare the port, if no environment port so it is 5000
 const PORT = process.env.PORT || 5000;
